e6f717dbfb206287bd8b4b431816b2b4fa6b776e
Sampathkumar <Sourab.Sampathkumar@CenturyLink.com>
2019-02-25 17:09:40 +0530

    Django remvoed

--- Languages/Python/Django/djangoproject/djangoproject/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.conf.urls import url, include
-
-urlpatterns = [
-    url('admin/', admin.site.urls),
-    url(r'^posts/', include('posts.urls'))
-]
--- Languages/Python/Django/djangoproject/posts/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from datetime import datetime
-
-
-# your models here.
-class Posts(models.Model):
-				title = models.CharField(max_length=30)
-				body = models.TextField()
-				created_at = models.DateTimeField(default=datetime.now, blank=True)
